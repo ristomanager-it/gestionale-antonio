721c5a0ee9f55d8df9f2d140369dbfbb5b9cf31c
ristomanager-it <ristoflowapp@gmail.com>
2025-12-28 10:10:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3002,7 +3002,7 @@ function renderRicetteViewer(lista, filtroTesto) {
     const card = document.createElement("div");
     card.className = "timbratura-intro-card";
     card.style.cursor = "pointer";
-await caricaIngredientiRicettaViewer(ricetta.id, card);
+caricaIngredientiRicettaViewer(ricetta.id, card);
 
     const base = r.pezzi_base || 0;
     const f1Perc = r.formato1_percent || 100;
