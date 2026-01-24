382c825864ae837d6fe4c8ea58b5a2f12d178090
ristomanager-it <ristoflowapp@gmail.com>
2026-01-24 21:59:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2375,8 +2375,8 @@ currentUser = null;
       giacenza: 0,
       scorta_minima: 5
     }
-  ],
-  sotto_scorta: [
+ 
+  sotto_scorta: [ ],
     {
       prodotto_id: 12,
       descrizione: "Farina 00",
