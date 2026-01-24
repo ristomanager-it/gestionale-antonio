dafcd6bdd5b1b1fe205016ef2027faf50791b733
ristomanager-it <ristoflowapp@gmail.com>
2026-01-24 17:41:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+// app.js
+document.addEventListener("DOMContentLoaded", () => {
+  const supabase = window.supabaseClient;
 
+  const CURRENT_USER_KEY = "ga_current_user_v1";
+  const THEME_KEY = "ga_theme_v1";
 
 // ===========================================================
 // ================== DOMContentLoaded =======================
