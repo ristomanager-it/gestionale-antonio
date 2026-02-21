ea3fcafa7f34080d9d7fd138c1847bf2b45d9013
ristomanager-it <ristoflowapp@gmail.com>
2026-02-21 20:47:23 +0100

    Update preventivi.js

--- js/views/preventivi.js
+++ js/views/preventivi.js
@@ -1,7 +1,3 @@
-// ============================================================
-// PREVENTIVI – Lista e Dettaglio Preventivo
-// ============================================================
-
 let preventiviCache = [];
 let filteredPreventivi = [];
 
