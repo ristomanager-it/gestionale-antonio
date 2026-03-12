dffc0619acf535d1f83e44d3dee4ba9e5ff2517c
ristomanager-it <ristoflowapp@gmail.com>
2026-03-12 22:14:06 +0100

    Update home.js

--- js/views/home.js
+++ js/views/home.js
@@ -1,23 +1,32 @@
 export async function render(container) {
 
-  const ruolo = window.state?.ruolo;
+  let ruolo = window.state?.ruolo;
   const reparto = window.state?.repartoAttivo?.nome;
 
-  if (ruolo === "superadmin") {
-    const mod = await import("./home-admin.js");
-    return mod.render(container);
+  // Se il superadmin sta simulando una vista
+  if (ruolo === "superadmin" && window.state?.viewAs) {
+    ruolo = window.state.viewAs;
   }
 
+  // Admin
   if (ruolo === "admin") {
     const mod = await import("./home-admin.js");
     return mod.render(container);
   }
 
+  // Manager
   if (ruolo === "manager") {
     const mod = await import("./home-manager.js");
     return mod.render(container, reparto);
   }
 
-  const mod = await import("./home-operatore.js");
+  // Operatore
+  if (ruolo === "operatore") {
+    const mod = await import("./home-operatore.js");
+    return mod.render(container);
+  }
+
+  // Default sicurezza
+  const mod = await import("./home-admin.js");
   return mod.render(container);
 }
