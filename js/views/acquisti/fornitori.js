d0f9d74517e09a816cd784d04ee0c8c775892f8b
ristomanager-it <ristoflowapp@gmail.com>
2026-03-13 08:49:21 +0100

    Create fornitori.js

---
+++ js/views/acquisti/fornitori.js
@@ -0,0 +1,8 @@
+export async function renderFornitori(container) {
+  container.innerHTML = `
+  <div class="card">
+    <h3>Fornitori</h3>
+    <p>Modulo fornitori in sviluppo</p>
+  </div>
+  `;
+}
