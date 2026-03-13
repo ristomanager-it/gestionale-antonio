58a43d1397ae128b8f7c05f3b52332be1f4a2137
ristomanager-it <ristoflowapp@gmail.com>
2026-03-13 08:49:55 +0100

    Create riordino.js

---
+++ js/views/acquisti/riordino.js
@@ -0,0 +1,8 @@
+export async function renderRiordino(container) {
+  container.innerHTML = `
+  <div class="card">
+    <h3>Riordino automatico</h3>
+    <p>Modulo riordino in sviluppo</p>
+  </div>
+  `;
+}
