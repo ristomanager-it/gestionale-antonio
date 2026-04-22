c9688779ff128f84af77b8f756695ab0dd3f1dcc
ristomanager-it <ristoflowapp@gmail.com>
2026-04-22 20:12:35 +0200

    Update index.js

--- js/views/prenotazioni/index.js
+++ js/views/prenotazioni/index.js
@@ -1488,13 +1488,6 @@ return String(phone || "").replace(/[^\d+]/g, "");
 }
 
 function escapeHtml(value) {
-  return String(value ?? "")
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-}
 
 function escapeAttribute(value) {
 return escapeHtml(value);
