dfa3194c3debb2fdf30e43628ab37f1343de2fc5
ristomanager-it <ristoflowapp@gmail.com>
2026-04-29 10:31:59 +0200

    Fix missing route for 'bo-ricette'

--- js/router.js
+++ js/router.js
@@ -125,7 +125,7 @@ const routes = {
   "bo-magazzino": () => import("./views/bo/bo-magazzino.js"),
   "bo-produzione": () => import("./views/bo/bo-produzione.js"),
   "bo-comande": () => import("./views/bo/bo-comande.js")
-     "bo-ricette": () => import("./views/bo/ricette-editor.js"),
+  "bo-ricette": () => import("./views/bo/ricette-editor.js"),
 
 }; // 🔴 QUESTA MANCAVA
 
