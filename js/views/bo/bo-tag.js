664db84aa792ddfde38ba5dca6034055504debb4
ristomanager-it <ristoflowapp@gmail.com>
2026-05-13 11:03:25 +0200

    Create bo-tag.js

---
+++ js/views/bo/bo-tag.js
@@ -0,0 +1,8 @@
+export async function render(container) {
+  container.innerHTML = `
+    <section class="view">
+      <h2>Gestione Tag</h2>
+      <p class="small-muted">Modulo in preparazione. La rotta è stabile e protetta dal router.</p>
+    </section>
+  `;
+}
