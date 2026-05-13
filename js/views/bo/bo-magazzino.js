81afc7c06610e62215ed19506d9bd03f8d9132e0
ristomanager-it <ristoflowapp@gmail.com>
2026-05-13 11:02:38 +0200

    Create bo-magazzino.js

---
+++ js/views/bo/bo-magazzino.js
@@ -0,0 +1,8 @@
+export async function render(container) {
+  container.innerHTML = `
+    <section class="view">
+      <h2>Back Office Magazzino</h2>
+      <p class="small-muted">Modulo in preparazione. La rotta è stabile e protetta dal router.</p>
+    </section>
+  `;
+}
