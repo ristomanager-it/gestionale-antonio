0a716ff7ef471496369bad84881394d60d3aa340
ristomanager-it <ristoflowapp@gmail.com>
2026-05-13 11:04:07 +0200

    Create bo-template.js

---
+++ js/views/bo/bo-template.js
@@ -0,0 +1,8 @@
+export async function render(container) {
+  container.innerHTML = `
+    <section class="view">
+      <h2>Template</h2>
+      <p class="small-muted">Modulo in preparazione. La rotta è stabile e protetta dal router.</p>
+    </section>
+  `;
+}
