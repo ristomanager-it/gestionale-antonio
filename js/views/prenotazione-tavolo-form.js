7c467c08660154e750a19bf3a0b5d4445a2f75c7
ristomanager-it <ristoflowapp@gmail.com>
2026-05-13 22:46:19 +0200

    Update import paths for contact and automation services

--- js/views/prenotazione-tavolo-form.js
+++ js/views/prenotazione-tavolo-form.js
@@ -1,5 +1,5 @@
-import { trovaOCreaContatto } from "/services/contatti.js";
-import { eseguiAutomazioni } from "/services/automazioni.js";
+import { trovaOCreaContatto } from "../services/contatti.js";
+import { eseguiAutomazioni } from "../services/automazioni.js";
 
 export async function render(container) {
 
