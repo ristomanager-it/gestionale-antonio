b99bd2f6af108e0428c6384e5b121c33e760a566
ristomanager-it <ristoflowapp@gmail.com>
2026-06-11 14:41:21 +0200

    Update router.js

--- js/router.js
+++ js/router.js
@@ -109,7 +109,7 @@ const routes = {
 
   preventivi: () => import("./views/preventivi.js"),
   creaPreventivo: () => import("./views/crea-preventivo.js"),
-  ai: () => import("./views/ai.js"),
+  ai: () => import("./views/ai.js?v=3"),
 
   permessi: () => import("./views/permessi-ferie.js"),
 
