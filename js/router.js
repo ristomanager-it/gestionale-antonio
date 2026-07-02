6d513182a690e91d25975ba8cd4ed16e720a8bf7
ristomanager-it <ristoflowapp@gmail.com>
2026-07-02 20:00:54 +0200

    Cache-bust bo-prodotti v4->v5

--- js/router.js
+++ js/router.js
@@ -169,7 +169,7 @@ const routes = {
   "bo-menu": () => import("./views/bo/bo-menu-builder.js?v=12"),
   "bo-fidelity": () => import("./views/bo/bo-fidelity.js"),
   "bo-categorie": () => import("./views/bo/categorie.js?v=2"),
-  "bo-prodotti": () => import("./views/bo/prodotti.js?v=4"),
+  "bo-prodotti": () => import("./views/bo/prodotti.js?v=5"),
 
   // PRODUZIONE
   "bo-magazzino": () => import("./views/bo/bo-magazzino.js"),
