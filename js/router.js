8ac2ff547e4ea9a491349bad9c3abdff2d8bee57
ristomanager-it <ristoflowapp@gmail.com>
2026-07-03 08:26:59 +0200

    chore: cache-bust display-cucina.js v=5

--- js/router.js
+++ js/router.js
@@ -192,7 +192,7 @@ const routes = {
   // =========================
   // DISPLAY (tablet fissi)
   // =========================
-  "display-cucina": () => import("./views/display/display-cucina.js?v=4"),
+  "display-cucina": () => import("./views/display/display-cucina.js?v=5"),
 
 }; // 
 
