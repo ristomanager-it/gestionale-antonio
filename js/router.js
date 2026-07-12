ed0ef0a6f44bcb807122f9ec593a8f3c6a464296
ristomanager-it <ristoflowapp@gmail.com>
2026-07-12 11:00:46 +0200

    router: bump scanner rinominato

--- js/router.js
+++ js/router.js
@@ -112,9 +112,9 @@ const routes = {
   completaAzienda: () => import("./views/completa-azienda.js"),
 "scegli-sede": () => import("./views/scegli-sede.js"),
   acquisti: () => import("./views/acquisti/index.js?v=2"),
-  magazzino: () => import("./views/magazzino/magazzino.js?v=6"),
+  magazzino: () => import("./views/magazzino/magazzino.js?v=7"),
   ordini: () => import("./views/ordini.js?v=2"),
-  ordine: () => import("./views/ordine.js?v=6"),
+  ordine: () => import("./views/ordine.js?v=7"),
 
   produzione: () => import("./views/produzione.js"),
   storicoLotto: () => import("./views/storico-lotto.js"),
