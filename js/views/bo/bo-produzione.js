fc6184668135d512143f76ccd53c1832c4da9106
ristomanager-it <ristoflowapp@gmail.com>
2026-07-12 19:20:58 +0200

    Rimozione modulo morto

--- js/views/bo/bo-produzione.js
+++
@@ -1,8 +0,0 @@
-export async function render(container) {
-  container.innerHTML = `
-    <section class="view">
-      <h2>Back Office Produzione</h2>
-      <p class="small-muted">Modulo in preparazione. La rotta è stabile e protetta dal router.</p>
-    </section>
-  `;
-}
