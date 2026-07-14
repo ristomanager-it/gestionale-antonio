91f073ea32ee4106a315fc723e559471e35385b6
ristomanager-it <ristoflowapp@gmail.com>
2026-07-14 13:18:48 +0200

    router: acquisti index v=4 (semaforo categorie fatture)

--- js/router.js
+++ js/router.js
@@ -112,7 +112,7 @@ const routes = {
   profilo: () => import("./views/completa-profilo.js"),
   completaAzienda: () => import("./views/completa-azienda.js"),
 "scegli-sede": () => import("./views/scegli-sede.js"),
-  acquisti: () => import("./views/acquisti/index.js?v=3"),
+  acquisti: () => import("./views/acquisti/index.js?v=4"),
   magazzino: () => import("./views/magazzino/magazzino.js?v=7"),
   ordini: () => import("./views/ordini.js?v=2"),
   ordine: () => import("./views/ordine.js?v=7"),
