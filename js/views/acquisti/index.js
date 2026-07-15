e4cf4242e843e1198e2648769ed27fa18a1d5133
ristomanager-it <ristoflowapp@gmail.com>
2026-07-15 08:53:41 +0200

    cache-bust fatture.js v=3

--- js/views/acquisti/index.js
+++ js/views/acquisti/index.js
@@ -1,4 +1,4 @@
-import { renderFatture } from "./fatture.js?v=2";
+import { renderFatture } from "./fatture.js?v=3";
 import { renderDDT } from "./ddt.js";
 import { renderPagamenti } from "./pagamenti.js";
 import { renderFornitori } from "./fornitori.js";
