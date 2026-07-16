ae1d8241b21f78fd12ee69fa16d9d4cb4d613a3a
ristomanager-it <ristoflowapp@gmail.com>
2026-07-16 08:44:40 +0200

    bust fatture v=10

--- js/views/acquisti/index.js
+++ js/views/acquisti/index.js
@@ -1,4 +1,4 @@
-import { renderFatture } from "./fatture.js?v=9";
+import { renderFatture } from "./fatture.js?v=10";
 import { renderDDT } from "./ddt.js";
 import { renderPagamenti } from "./pagamenti.js";
 import { renderFornitori } from "./fornitori.js";
