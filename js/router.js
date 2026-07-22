ad1ec6793a21eb742145da484c0729824b09427b
ristomanager-it <ristoflowapp@gmail.com>
2026-07-22 18:21:57 +0200

    Cache-bust crea-ricetta v27 + router v201 (avatar Tony live)

--- js/router.js
+++ js/router.js
@@ -77,7 +77,7 @@ const routes = {
   amministrazione: () => import("./views/amministrazione.js"),
   gestione: () => import("./views/gestione.js"),
   "ricette-semplici": () =>
-    import("./views/crea-ricetta.js?v=26"),
+    import("./views/crea-ricetta.js?v=27"),
   "permessi-operatore": () =>
     import("./views/permessi-operatore/index.js"),
 
@@ -129,9 +129,9 @@ const routes = {
   ricettario: () => import("./views/ricettario.js?v=5"),
   "planner-produzione": () => import("./views/planner-produzione.js"),
   // Editor ricette UNICO: modalità semplice/avanzata dentro crea-ricetta.js
-  creaRicetta: () => import("./views/crea-ricetta.js?v=26"),
-  "crea-ricetta":          () => import("./views/crea-ricetta.js?v=26"),
-  "crea-ricetta-avanzata": () => import("./views/crea-ricetta.js?v=26"),
+  creaRicetta: () => import("./views/crea-ricetta.js?v=27"),
+  "crea-ricetta":          () => import("./views/crea-ricetta.js?v=27"),
+  "crea-ricetta-avanzata": () => import("./views/crea-ricetta.js?v=27"),
   "abbina-articoli": () => import("./views/abbina-articoli.js"),
   "menu-engineering": () => import("./views/menu-engineering.js?v=2"),
   "super-tony": () => import("./views/super-tony.js?v=6"),
