e1e5eeec74350935eaa8f301dcd547381e6ac5f0
ristomanager-it <ristoflowapp@gmail.com>
2026-08-06 23:50:31 +0200

    Patch js/views/bo/bo-calendario.js

--- js/views/bo/bo-calendario.js
+++ js/views/bo/bo-calendario.js
@@ -241,7 +241,7 @@ async function carica() {
 
   const { data: giorni, error } = await supa()
     .from('v_calendario_esito')
-    .select('id,data,angolo,stato,tema,titolo,testo,media_url,grafica_url,grafica_modo,tema_grafico,immagine_finale,istruzioni,ricorrenza,tipo_giorno,verificata,esito')
+    .select('id,data,angolo,stato,tema,titolo,testo,media_url,grafica_url,grafica_modo,tema_grafico,immagine_finale,istruzioni,sede_id,ricorrenza,tipo_giorno,verificata,esito')
     .eq('azienda_id', az)
     .eq('canale', 'facebook')
     .gte('data', iso(dal))
