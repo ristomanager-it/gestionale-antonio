c67c4992aa81858a4d1c789c3be0d17b20663333
ristomanager-it <ristoflowapp@gmail.com>
2026-08-07 14:19:20 +0200

    Patch js/components/camera.js

--- js/components/camera.js
+++ js/components/camera.js
@@ -512,6 +512,7 @@ async function chiediPost(mediaId, url, isVideo) {
     '<div class="cam-barra cam-azioni">' +
       '<button class="cam-a sec" id="cam-fine">Solo galleria</button>' +
       (isVideo ? '' : '<button class="cam-a sto" id="cam-storia">📱 Storia</button>') +
+      (isVideo ? '' : '<button class="cam-a ric" id="cam-ricetta">📖 Ricetta</button>') +
       '<button class="cam-a pri" id="cam-post">Fanne un post</button>' +
     '</div>';
 
